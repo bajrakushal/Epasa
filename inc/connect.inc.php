3b8da6d84fb8a90015353e8b7313ea28b26c9ce0
Rahi <md.alshahriarrahi@gmail.com>
2020-10-08 00:46:40 +0600

    keeping a backup

---
+++ inc/connect.inc.php
@@ -0,0 +1,5 @@
+<?php
+	Global $conn;
+	$conn = mysqli_connect("localhost","root","") or die("Couldn't connet to SQL server");
+	mysqli_select_db($conn, "ebuybd") or die("Couldn'ttt select DB");
+?>
